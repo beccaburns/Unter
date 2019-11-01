9298127b9dff3cb0d924a3be9aae98d34f95db83
Becca <beccaburns89@gmail.com>
2019-11-01 15:12:07 +0100

    Sets up Rails app scaffolding. Adds Cucumber for acceptance testing and RSpec for unit testing application.

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Unter
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
+  end
+end
